820d654c4154a93030da1a2a0beb974846be5553
wasim <wasim.hannure@yahoo.in>
2024-09-08 15:49:54 +0530

    git actions

--- Terraform-VPC/provider.tf
+++ Terraform-VPC/provider.tf
@@ -5,14 +5,7 @@ terraform {
       version = "5.16.1"
     }
   }
-
-  backend "s3" {
-    bucket = "terraform-remote-backend-s3-save"
-    key    = "dev/terraform.tfstate"
-    region = "ap-south-1"
-  }
 }
-
 provider "aws" {
   region = "ap-south-1"
 }
